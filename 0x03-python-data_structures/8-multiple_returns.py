f52ff626f7ff7deb51c209234d6bb8c66f339983
Basil Njoga <Basilnjoga@gmail.com>
2022-06-07 22:00:24 +0300

    check

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    sent_tuple = (len(sentence), sentence[0])
+    if len(sentence) == 0:
+        first = None
+    else:
+        first = sentence[0]
+    sent_tuple = (len(sentence), first)
     return sent_tuple
