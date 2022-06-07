00ebfd669f18ff5719fc282c56290924b80667c0
Basil Njoga <Basilnjoga@gmail.com>
2022-06-07 20:57:30 +0300

    check

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,13 +2,12 @@
 
 def no_c(my_string):
     new_string = list(my_string)
+    line = []
     for x in range(len(new_string)):
         if new_string[x] == "c" or new_string[x] == "C":
-            new_string[x] = " "
+            line.append(x)
+    for y in range(len(line)):
+        new_string.remove(line[y])
     s = ""
     s = s.join(new_string)
     return s
-
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
