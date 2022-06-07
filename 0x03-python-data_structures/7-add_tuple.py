3cda6debed923b455b3e177ecbcac95721b4214e
Basil Njoga <Basilnjoga@gmail.com>
2022-06-07 21:50:11 +0300

    changed functionality

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 1:
+    len1 = len(tuple_a)
+    len2 = len(tuple_b)
+    if len1 == 1 and len2 == 2:
         tuple_c = (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
-    elif len(tuple_a) == 0:
+    elif len1 == 0 and len2 == 2:
         tuple_c = (0 + tuple_b[0], 0 + tuple_b[1])
-    elif len(tuple_b) == 1:
+    elif len2 == 1 and len1 == 2:
         tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
-    elif len(tuple_b) == 0:
+    elif len2 == 0 and len1 == 2:
         tuple_c = (tuple_a[0] + 0, tuple_a[1] + 0)
-    elif len(tuple_a) == 1 and len(tuple_b) == 1:
+    elif len1 == 1 and len2 == 1:
         tuple_c = (tuple_a[0] + tuple_b[0], 0 + 0)
-    elif len(tuple_a) == 0 and len(tuple_b) == 0:
+    elif len1 == 0 and len2 == 0:
         tuple_c = (0 + 0, 0 + 0)
     else:
         tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return tuple_c
+
+tuple_a = ()
+tuple_b = ()
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
