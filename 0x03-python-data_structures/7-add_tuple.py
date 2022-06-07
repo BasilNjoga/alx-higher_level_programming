00d10b00c03d390d11416267153981c9e47c3abc
Basil Njoga <Basilnjoga@gmail.com>
2022-06-07 21:50:49 +0300

    recheck

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -18,8 +18,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
     else:
         tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return tuple_c
-
-tuple_a = ()
-tuple_b = ()
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
