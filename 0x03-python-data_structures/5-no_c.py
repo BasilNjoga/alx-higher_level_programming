2b5ed41dab1150094781a74c910bdf690ebc626d
Basil Njoga <Basilnjoga@gmail.com>
2022-06-07 17:55:46 +0300

    check

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-def no_c(my_string):
-    for x in my_string:
-        if my_string[x] == 'c' or my_string[x] == 'C':
-            my_string[x] == ' '
+#def no_c(my_string):
+my_string = "chlamediac"
+for x in my_string:
+    if x == "c" or "C":
+        x = " "
+
+print(my_string)
