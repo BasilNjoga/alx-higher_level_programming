baacd1a8a6b6ca5332504f316905ca36047d9153
Basil Njoga <Basilnjoga@gmail.com>
2022-06-08 16:27:02 +0300

    check

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_list = matrix * 1
+    new_list = matrix[] * 1 
     for x in range(len(new_list)):
         for y in range(len(new_list[x])):
             new_list[x][y] = new_list[x][y]**2
