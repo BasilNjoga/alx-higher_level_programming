e2b5cb6a9a424fc945428d754c9080c8953908fa
Basil Njoga <Basilnjoga@gmail.com>
2022-06-11 02:16:56 +0300

    check

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictonary):
+    new_dictionary = {}
+    new_list = list(a_dictionary)
+    for i in new_list:
+        new_dictionary = dict([i, a_dictionary[i] * 2])
+    return new_dictionary
+
