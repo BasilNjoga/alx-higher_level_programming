7b5bbe04781cb7920908c9e7f7e9ad999c77df01
Basil Njoga <Basilnjoga@gmail.com>
2022-06-12 01:51:48 +0300

    range rather than string

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    for i in str:
-        n = ord(i)
-        if n > 90:
-            n = n - 32
+    for i in range(len(str)):
+        if i < len(str) - 1:
+            n = ord(str[i])
+            if n > 90:
+                n = n - 32
+            else:
+                n = n
+            print("{}".format(chr(n)), end='')
         else:
-            n = n
-        print("{}".format(chr(n)),end='')
-    print("\n")
+            n = ord(str[len(str) - 1])
+            if n > 90:
+                n = n - 32
+            else:
+                n = n
+            print("{}".format(chr(n)))
