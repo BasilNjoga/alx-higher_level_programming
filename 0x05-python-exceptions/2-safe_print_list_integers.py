fe4755d81f608bb803f8c8b1c6f1e1d546c72188
Basil Njoga <Basilnjoga@gmail.com>
2022-06-21 00:08:47 +0300

    check

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    j = 0
+    z = 0
+    y = []
+    while x > 0:
+        try:
+            k = my_list[j] / 3
+            y.append(my_list[j])
+            y.clear
+            print("{}".format(my_list[j]), end="")
+            z = z + 1
+        except TypeError:
+            pass
+        j = j + 1
+        x = x - 1
+    print("\n", end="")
+    return(z)
