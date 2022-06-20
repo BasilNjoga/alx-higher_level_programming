5ade221b5e4803d5f8905213f9101d9330b87481
Basil Njoga <Basilnjoga@gmail.com>
2022-06-20 22:42:44 +0300

    check

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    j = 0
+    y = []
+    try:
+        y.append(my_list[x-1])
+        y.clear()
+        while x > 0:
+            print("{}".format(my_list[j]),end="")
+            j = j + 1
+            x = x - 1
+        print("\n",end="")
+        return(j)
+    except:
+        for i in my_list:
+            print("{}".format(i),end="")
+            j = j + 1
+        print("\n",end="")
+        return(j)
+
+
+
+
+
