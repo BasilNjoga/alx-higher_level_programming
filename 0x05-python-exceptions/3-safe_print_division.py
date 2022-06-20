55068dcdcfb315bf453241cebfe9abdab79c558e
Basil Njoga <Basilnjoga@gmail.com>
2022-06-21 00:39:48 +0300

    check

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,17 @@
+#!/bin/usr/python3
+
+def safe_print_division(a, b):
+    try:
+        result = a / b
+        return (result)
+    except ZeroDivisionError:
+        return (None)
+    finally:
+        try:
+            result = a / b
+            return(result)
+        except:
+            result = None
+        print("Inside result: {}".format(result))
+        #return(result)
+
