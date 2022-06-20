83faf48c8befbcf9d35c606e3654df2f681cc4de
Basil Njoga <Basilnjoga@gmail.com>
2022-06-20 22:52:26 +0300

    check

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,22 +3,19 @@
 def safe_print_list(my_list=[], x=0):
     j = 0
     y = []
-    if x == 0:
-        return(0)
-    else:
-        try:
-            y.append(my_list[x-1])
-            y.clear()
-            while x > 0:
-                print("{}".format(my_list[j]),end="")
-                j = j + 1
-                x = x - 1
-            print("\n",end="")
-            return(j)
-        except:
-            for i in my_list:
-                print("{}".format(i),end="")
-                j = j + 1
-            print("\n",end="")
-            return(j)
+    try:
+        y.append(my_list[x-1])
+        y.clear()
+        while x > 0:
+            print("{}".format(my_list[j]),end="")
+            j = j + 1
+            x = x - 1
+        print("\n",end="")
+        return(j)
+    except:
+        for i in my_list:
+            print("{}".format(i),end="")
+            j = j + 1
+        print("\n",end="")
+        return(j)
 
