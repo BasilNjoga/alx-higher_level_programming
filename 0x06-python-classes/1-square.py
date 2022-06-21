265bfdf530a8a9cda3bdc7d1041f4c03362b7250
Basil Njoga <Basilnjoga@gmail.com>
2022-06-21 18:33:37 +0300

    check

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This is a module containing a class square
+This class only has attributes at the present 
+
+Attributes:
+    class Square
+
+"""
+
+
+class Square:
+    """an empty Square class
+
+    Attributes:
+        attr1 : size
+            size of the square
+
+    Args:
+        None
+    """
+    __size = None
