58eef8248e5e87da2dfd777857baddf82de7e1b6
Basil Njoga <Basilnjoga@gmail.com>
2022-06-21 18:15:43 +0300

    hceck

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This is a module containing an empyt class square
+class which will be used later to do other things
+
+Attributes:
+    None at the moment
+
+"""
 
 class Square:
     """an empty Square class
