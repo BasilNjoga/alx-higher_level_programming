861c28544e36b5be4a2233b2bcb66553e57965a8
Basil Njoga <Basilnjoga@gmail.com>
2022-06-27 21:32:37 +0300

    check

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This is a module containing an empty class rectangle
+class which will be used later to do other things
+
+Attributes:
+    None at the moment
+
+"""
+
+
+class Rectangle:
+    """an empty Rectangle class
+
+    Attributes:
+        None
+
+    Args:
+        None
+    """
+    pass
