52d01d38d3ada0f8ac69bf2d5b54871d462c12c5
Basil Njoga <Basilnjoga@gmail.com>
2022-06-28 00:17:08 +0300

    check

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+This is a module based on 0-rectangles.pym that contains a class that defines a rectangle
+giving it's height and width
+
+Attributes:
+    __init__ : first instance 
+
+"""
+
+class Rectangle:
+    """
+    This is a class that defines a rectangle giving 
+    it's width and heigth
+
+    Attributes:
+        __init__: first instance
+
+    Args:
+        self:
+    """
+    def __init__(self,width=0,height=0):
+        
