243c131243f896201a6fa27a9cde7cf4f5d6334b
Basil Njoga <Basilnjoga@gmail.com>
2022-06-30 19:35:00 +0300

    say my name

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+    This is a module about matrix divided and includes a single 
+    function matrix_divided
+    
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    It divides a two dimensional matrix by a factor v
+    and returns a new matrix with the new values
+    Args:
+        matrix: The two dimensional matrix
+        div:number to be divided by
+    """
+    if not isinstance(matrix,list):
+        raise TypeError("matrix must be a matrix(list of lists) of integers/floats")
+    new_matrix = list(map(list, matrix))
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            new_matrix[i][j] = round(new_matrix[i][j] / div,2)
+
+    return new_matrix
