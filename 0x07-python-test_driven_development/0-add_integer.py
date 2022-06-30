3948db5333108d0dc67365dce85ab378e0294a71
Basil Njoga <Basilnjoga@gmail.com>
2022-06-30 18:29:34 +0300

    check

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,13 +7,14 @@ it has a single function, add_integers() which adds two integers
 
 """
 
+
 def add_integer(a, b=98):
     """
     it adds two integers and returns the sum
 
     """
-    if isinstance(a,str):
+    if isinstance(a, str):
         raise TypeError("a must be an integer")
-    if isinstance(b,str):
+    if isinstance(b, str):
         raise TypeError("b must be an integer")
-    return int (a + b)
+    return int(a + b)
