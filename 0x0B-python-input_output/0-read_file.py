b6b41ed0e27c0947bb1352d16f5426232efbe6a1
Basil Njoga <Basilnjoga@gmail.com>
2022-07-05 21:37:13 +0300

    check

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This is a module on python input outputs
+This file has a single function that reads data
+from a text file
+"""
+
+
+def read_file(filename=""):
+    """
+    This program simply reads text from a file
+    """
+    with open(filename,encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
