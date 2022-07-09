0c3646c405321fe4694a2ce11abf37ca6cec6d23
Basil Njoga <Basilnjoga@gmail.com>
2022-07-09 14:01:53 +0300

    check

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -10,7 +10,5 @@ def write_file(filename="", text=""):
     """
     function that writes to a file
     """
-    filename = "newfile"
     with open(filename, mode="w", encoding="utf-8") as f:
-        f.write(text)
-
+        return (f.write(text))
