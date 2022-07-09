eabb7956d9ac1a40f1c9f38bb86b36468c463e64
Basil Njoga <Basilnjoga@gmail.com>
2022-07-09 14:04:34 +0300

    check

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 """
-This is module that appends a string to 
+This is module that appends a string to
 the end of a file
 """
 
+
 def append_write(filename="", text=""):
     """
-    This is a function that appends to a 
+    This is a function that appends to a
     string
     """
     with open(filename, mode="a", encoding="utf-8") as f:
-        f.write(text)
-
+        return(f.write(text))
