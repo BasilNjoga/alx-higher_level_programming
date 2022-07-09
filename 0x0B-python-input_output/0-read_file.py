f4aa1127f8c9702a241fc2881fddf0e6759baf02
Basil Njoga <Basilnjoga@gmail.com>
2022-07-09 13:28:49 +0300

    check

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+This is a module on input and output functions in python
+this module contains a single function read_file that 
+prints out the contents of a file
+"""
 
 def read_file(filename=""):
+    """
+    This module simply prints out the
+    contents of a file
+    """
     with open(filename, mode="r", encoding="utf-8") as f:
         print(f.read())
