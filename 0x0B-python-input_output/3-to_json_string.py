932ed12da4a8669cdc180af57eb688454c7a6ecc
Basil Njoga <Basilnjoga@gmail.com>
2022-07-09 15:52:26 +0300

    check

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+"""
+This is module on Json
+it simply returns a json representation
+"""
+
+
+def to_json_string(my_obj):
+    """
+    This is a function that returns a function 
+    representation of a string
+    """
+    json.dumps(my_obj)
