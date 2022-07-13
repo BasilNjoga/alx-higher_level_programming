f1b88448eef5c2bac430ccf8153e99eb7331167e
Basil Njoga <Basilnjoga@gmail.com>
2022-07-13 20:03:38 +0300

    check

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,11 +5,11 @@ it simply returns a json representation
 """
 import json
 
+
 def to_json_string(my_obj):
     """
-    This is a function that returns a function 
+    This is a function that returns a function
     representation of a string
     """
     d1 = json.dumps(my_obj)
     return d1
-
