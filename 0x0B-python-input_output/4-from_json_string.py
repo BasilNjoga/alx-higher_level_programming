549041fd6e507940a9c4f5f4428b6e85f394697f
Basil Njoga <Basilnjoga@gmail.com>
2022-07-13 20:06:31 +0300

    check

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is a modole on python input outputs 
+along with json decoding and encoding
+"""
+
+def from_json_string(my_str):
+    """
+    This is a function that returns a json
+    representation of a string in object form
+    """
+    return json.loads(my_str)
