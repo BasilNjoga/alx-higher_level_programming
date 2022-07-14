bac20687931571b2a663203ea3546054cbb8d203
Basil Njoga <Basilnjoga@gmail.com>
2022-07-14 19:01:38 +0300

    check

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """
 This is a module called base that has a single
-class base which will be the base of all 
+class base which will be the base of all
 future classes
 """
+
 
 class Base:
     """
     This is a class that contains a private attribute nb_object along with
     public attributes self and None
     """
-    __nb_object = 0
-    def __init__(self,id=None):
+
+    def __init__(self, id=None):
         self.__nb_object = 0
-        if id == None:
+        if id is None:
             self.__nb_object += 1
-            self.id =self. __nb_object
+            self.id = self. __nb_object
         else:
             self.id = id
