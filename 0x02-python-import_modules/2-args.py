64e39beda07a60a21aaf018ce1be1c9f24877c77
Basil Njoga <Basilnjoga@gmail.com>
2022-08-09 01:01:16 +0300

    check

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 def print_arg(argv):
 	n = len(argv) - 1
 	if n == 0:
-		print("{:d} argument.".format(n))
+        print("{:d} argument.".format(n))
 		return
 	else:
 		if n == 1:
@@ -13,6 +14,7 @@ def print_arg(argv):
 		while i <= n:
 			print("{:d}: {:s}".format(i, argv[i]))
 			i += 1
+
 
 if __name__ == "__main__":
 	import sys
