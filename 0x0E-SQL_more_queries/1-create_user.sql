f35f486d578a81325942ccd76983ce322ece0305
Basil <basilnjoga@gmail.com>
2022-08-11 15:31:41 -0700

    check

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- This script creates a new user
 -- This is the code to create a new user
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
