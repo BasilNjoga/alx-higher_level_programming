05f6ff70efdc7b77595760bb6b9d8909f9901a6b
Basil <basilnjoga@gmail.com>
2022-08-11 15:07:48 -0700

    check

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- This is a script that lists the priviledges of all the users
+-- This is the code
+SHOW GRANTS FOR 'user_0d_1', 'user0d_2' @ 'localhost'
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+This is a project on more queries on sql including joins and granting proviledge
