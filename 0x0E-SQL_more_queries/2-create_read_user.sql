bcbfc2b695b356692848e9c249e2564fba5c23c7
Basil <basilnjoga@gmail.com>
2022-08-11 16:10:22 -0700

    check

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- This script createsa  database along with a user
 -- This is both lines of the code
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2 ;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'i;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
