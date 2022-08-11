7f6b8211b9e56d5c22fc45ef39b78b3c4395cd7b
Basil <basilnjoga@gmail.com>
2022-08-11 16:25:40 -0700

    check

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,3 @@
 -- This table creates a table
 -- This is the code
-CREATE TABLE IF NOT EXIST force_name (
-	id INT,
-	name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
