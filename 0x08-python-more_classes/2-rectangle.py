28e9cc064b1b505458c428823e77b19f01250bf9
Basil Njoga <Basilnjoga@gmail.com>
2022-08-17 01:56:50 +0300

    check

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -20,11 +20,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -35,8 +35,9 @@ class Rectangle:
 
     def area(self):
         return self.__width * self.__height
+
     def perimeter(self):
         if (self.__width == 0 or self.__height == 0):
             return 0
         else:
-            return 2(self.__width + self.__height)
+            return ((self.__width * 2) + (self.__height * 2))
