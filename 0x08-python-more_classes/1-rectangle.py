25457f1a6df19aa06874cf21f32cd031eb3f5b1c
Basil Njoga <Basilnjoga@gmail.com>
2022-08-17 00:37:44 +0300

    check

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,4 +21,19 @@ class Rectangle:
         self:
     """
     def __init__(self,width=0,height=0):
-        
+        self.width = width
+        self.height = height
+
+        @property
+        def width(self):
+            return self.__width
+        @width.setter
+        def width(self, width):
+            self.__width = width
+
+        @property
+        def height(self):
+            return self.__height
+        @height.setter
+        def height(self, height):
+            self.__height = height
