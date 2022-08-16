400a0358ba9a1ca67fee7197770923360dd05d53
Basil Njoga <Basilnjoga@gmail.com>
2022-08-16 23:47:25 +0300

    check

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,8 @@ This is a module that creats a classs that inherits from a passed object
 def print_sorted(self):
     class Mylist(lists):
         """ My list class that inherits from another """
+
+        
         def __init__(self):
             """ This is a super unit function"""
             super().__init__()
