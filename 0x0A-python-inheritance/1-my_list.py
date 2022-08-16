175cebf698ce4164a4217e16440b6b4c125d4ec0
Basil Njoga <Basilnjoga@gmail.com>
2022-08-16 23:42:59 +0300

    check

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-This is a module that creats a classs that inherits from a passed object 
+This is a module that creats a classs that inherits from a passed object
 """
 
 
 def print_sorted(self):
     """ This is a function that returns an inherited class """
     class Mylist(lists):
-        """ My list class """
+        """ My list class that inherits from another """
         def __init__(self):
             super().__init__()
-    
+
     u = MYlist()
     return u
