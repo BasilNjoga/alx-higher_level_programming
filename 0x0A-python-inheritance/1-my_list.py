2f996a2cc36b14c4ade355945919951f591e3758
Basil Njoga <Basilnjoga@gmail.com>
2022-08-21 01:37:28 +0300

    check

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python3
-""" This is a module that creats a classs that inherits from a passed object """
+""" This is a module that creats a classs that inherits
+from a passed object """
 
 
+class list:
+
+    def __init__(self):
+        self.numbers = []
+
+    def append(self, num):
+        self.numbers.append(num)
+
+    def __str__(self):
+        return str(self.numbers)
+
+
+class MyList(list):
+    """This is a list that inherits from another """
+    def print_sorted(self):
+        for j in range(len(self.numbers)):
+            for i in range(len(self.numbers) - 1):
+                if self.numbers[i] > self.numbers[i+1]:
+                    newlist = self.numbers[i]
+                    self.numbers[i] = self.numbers[i+1]
+                    self.numbers[i+1] = newlist
+        print(self.numbers)
