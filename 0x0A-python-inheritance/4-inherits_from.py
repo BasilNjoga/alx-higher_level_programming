2419c38b305345872079615282be0e727de2f1df
Basil Njoga <Basilnjoga@gmail.com>
2022-08-21 03:02:23 +0300

    check

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" This module only checks for inheritance """
+
+def inherits_from(obj, a_class):
+    """ This module checks for inheritance """
+    return(isinstance(obj, a_class))
