fa5b1005701badab9fb58e4a5c3ffd3499d223f5
Basil Njoga <basilnjoga@gmail.com>
2022-09-05 21:20:11 +0000

    check

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+let newstr;
+
+if (Number(argv[2])) {
+  for (let j = 0; j < Number(argv[2]); j++) {
+    newstr = '';
+    for (let i = 0; i < Number(argv[2]); i++) {
+      newstr = newstr + 'x';
+    }
+    console.log(newstr);
+  }
+} else {
+  console.log('Missing size');
+}
