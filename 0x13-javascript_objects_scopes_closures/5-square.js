e4351a086b9e8403b8b3fd4389eafb973619e872
Basil Njoga <basilnjoga@gmail.com>
2022-09-08 01:24:47 +0000

    check

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let j = 0; j < this.height; j++) {
+      let myString = '';
+      for (let i = 0; i < this.width; i++) {
+        myString = myString + 'X';
+      }
+      console.log(myString);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
+
+module.exports = class square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
