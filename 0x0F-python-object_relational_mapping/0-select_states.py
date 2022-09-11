5ede43a30a366b84cd31bd262696208b90be8eaa
Basil <Basilnjoga@gmail.com>
2022-09-11 19:54:46 +0000

    check

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 #This is a problem set on python 
-db = MySQLdb.connect(host=MY_HOSt, user=MY_USER, passwd=MY_PASS, db = MY_DB)
+db = MySQLdb.connect(host="localhost" user="root", passwd="", db ="hbtn_0e_0_usa")
+
+cur = db.cursor()
+c.execute("""SELECT * FROM states """)
+
+
