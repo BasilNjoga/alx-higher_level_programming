1aa600ffd22285a071151ecf42dcf4dcc9ceb655
Basil Njoga <Basilnjoga@gmail.com>
2022-09-22 22:13:12 +0300

    ccheck

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """This is an algorithm to find the peak in a list """
 
-def fin_peak(list_of_integers):
+def find_peak(list_of_integers):
     """ This function finds the peak ina list"""
+    start = 2
+    middle = 3
+    end = 4
     mylist = list_of_integers
-    if (mylist == NULL):
+    if (mylist == None):
         return (NULL)
-    for i in range(len(mylist)):
-        if ((mylist[i] > mylist[i+1]) && (mylist[i] > mylist[i-1])):
+    for i in range(1, len(mylist) - 1):
+        if ((mylist[i] > mylist[i+1]) & (mylist[i] > mylist[i-1])):
             return mylist[i]
 
