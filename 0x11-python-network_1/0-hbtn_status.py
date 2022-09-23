42b6c08fc6df995491888c826bff8e067c269bab
Basil Njoga <Basilnjoga@gmail.com>
2022-09-23 18:25:04 +0300

    check

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This script fetches a url """
+import urlib.request
+
+if __name__ == "__main__":
+    with urlib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+
+
