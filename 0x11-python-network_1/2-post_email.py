44971067e158917c1535b569a48c90683339405c
basil <basilnjoga@gmail.com>
2022-10-23 04:56:31 -0700

    check

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" This script takes a url and email and sends a post request """
+
+from sys import argv
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    """ reads as a file """
+    url = argv[1]
+    values = { 'email' : argv[2]}
+
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+
+    print(response.getheader('email'))
