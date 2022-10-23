42ed76f16156d591c4e39d34485fe23f7f186c60
basil <basilnjoga@gmail.com>
2022-10-23 04:36:14 -0700

    chck

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""This script takes a url and displays the value of a header """
+""" This script takes a url and displays the value of a header """
+
 from sys import argv
 import urllib.request
 
 if __name__ == "__main__":
+    """ returns response as a file """
     with urllib.request.urlopen(argv[1]) as response:
         html = response.read()
 
-print(html.X-Request-Id)
+    print(html.X-Request-Id)
