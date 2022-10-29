8a2525e4fb7b3dac82618439dcbac6f4f7b8dcd1
Basil Njoga <Basilnjoga@gmail.com>
2022-10-29 19:58:24 +0300

    check

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+$(document).ready(function(){
+    $.get("https://www.fourtonfish.com/hellosalut/hello/");
+    $("INPUT#btn_translate").click(funciton(){
+
+    });
+    $("#DIVhello").text(data);
+});
