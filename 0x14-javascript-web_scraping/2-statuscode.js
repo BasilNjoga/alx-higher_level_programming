0320db226add8fd5ac2492d04ce267dd8215245c
basil <basilnjoga@gmail.com>
2022-11-13 15:43:19 +0000

    check

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (error) throw error;
-  console.log(response.statusCode);
+  console.log('code: ' + response.statusCode);
 });
