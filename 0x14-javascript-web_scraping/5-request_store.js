aa0e8b61fd45a0526c11a5404f0fab16692b5839
basil <basilnjoga@gmail.com>
2022-11-13 18:27:51 +0000

    check

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const fs = require('fs');
 
-request (process.argv[2], function (error, response, body) {
+request(process.argv[2], function (error, response, body) {
   if (error) throw error;
   fs.writeFile(process.argv[3], body, 'utf-8', function (error) {
     if (error) throw error;
