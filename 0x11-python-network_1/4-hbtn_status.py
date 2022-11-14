703bd294296cb5e372513fb44db9e32a50f8a455
basil <basilnjoga@gmail.com>
2022-11-14 01:51:09 -0800

    check

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,5 +4,5 @@ import requests
 
 r = requests.get('https://alx-intranet.hbtn.io/status')
 print("Body response:")
-print("\t - type: {}".format(type(r.text)))
-print("\t - content: {}".format(r.text))
+print("\t- type: {}".format(type(r.text)))
+print("\t- content: {}".format(r.text))
