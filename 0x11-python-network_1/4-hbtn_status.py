50bbb3399cccc650452af6cf8edf7d5a8c37f8f3
basil <basilnjoga@gmail.com>
2022-11-14 01:46:20 -0800

    check

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,5 +2,7 @@
 """ This is  script that fetches a url using requests """
 import requests
 
-if __name__ == "__main__":
-    r = requests.get('https://alx-intranet.hbtn.io/status')
+r = requests.get('https://alx-intranet.hbtn.io/status')
+print("Body response:")
+print("\t - type: {}".format(type(r.text)))
+print("\t - content: {}".format(r.text))
