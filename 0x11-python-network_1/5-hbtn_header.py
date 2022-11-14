e6e9a09db50d3c6cfc330b3f39b44553d7b2f6de
basil <basilnjoga@gmail.com>
2022-11-14 02:17:59 -0800

    check

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,7 +5,7 @@ from sys import argv
 
 if __name__ == "__main__":
     r = requests.get(argv[1])
-    if (r.headers['X-Request-Id'] == False):
+    if (r.headers['X-Request-Id'] is False):
         print(None)
     else:
         print(r.headers['X-Request-Id'])
