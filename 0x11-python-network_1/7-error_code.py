711f6b1a8747cb116ae61344d3bf3d064714661d
basil <basilnjoga@gmail.com>
2022-11-15 00:20:38 -0800

    check

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,3 +2,9 @@
 """ This script sends a request to the URL and displays the body of the response """
 import requests
 from sys import argv
+
+r = requests.get(argv[1])
+if (r.status_code >= 400):
+    print("Error: {}".format(r.status_code))
+else:
+    print(r.text)
