7a9b3efe2350437285649537d2e8b68dd578216f
basil <basilnjoga@gmail.com>
2022-11-15 00:14:32 -0800

    check

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" This script sends a a request and displays the status code"""
+""" 
+This script sends a a request and displays the status code required in the call
+"""
 
 import requests
 from sys import argv
