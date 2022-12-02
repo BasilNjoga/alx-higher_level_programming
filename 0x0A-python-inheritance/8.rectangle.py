181a04eb84d5b78e2829c7cddd1eba0f04c0c7cf
Basil Njoga <Basilnjoga@gmail.com>
2022-12-02 14:56:09 +0300

    check

--- 0x0A-python-inheritance/8.rectangle.py
+++ 0x0A-python-inheritance/8.rectangle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-    8-rectangle: class Rectangle from BaseGeomerty
-"""
+"""8-rectangle: class Rectangle from BaseGeomerty"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-    7-rectangle: class Rectangle from BaseGeomerty
-"""
+"""7-rectangle: class Rectangle from BaseGeomerty """
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
